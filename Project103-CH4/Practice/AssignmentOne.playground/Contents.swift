53f4b7c600d86a59ab97834ca3b084c968ce62e8
Wendell Richards <wenrich85@gmail.com>
2025-11-06 18:42:56 -0600

    Assignment 1

---
+++ Project103-CH4/Practice/AssignmentOne.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var studentNames: [String] = ["Wendell", "Alex", "Linda", "Ivan", "Ken"]
+
+var studentGrades: [String:[Int]] =
+            [
+                "Wendell": [84, 45, 55],
+                "Alex" : [99, 76, 90],
+                "Linda" : [98,78,85],
+                "Ivan" : [90, 98, 97],
+                "Ken" : [100, 85, 56]
+            ]
+
+studentGrades.forEach() { name, grades in
+    print("\(name) has the following grades \(grades) and an average of \(grades.reduce(0, +) / grades.count)")
+    
+}
